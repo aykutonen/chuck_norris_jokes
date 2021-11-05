e8e893731198f2ee266052694f85572b270552e5
aykutonen <aykutonen@gmail.com>
2021-11-05 23:59:21 +0300

    listeden silme işlemi dahil edildi.

--- lib/views/likes.dart
+++ lib/views/likes.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LikesView extends StatelessWidget {
-  final ctrl = Get.find<HomeController>();
+  final homeCtrl = Get.find<HomeController>();
 
   @override
   Widget build(BuildContext context) {
@@ -14,17 +14,37 @@ class LikesView extends StatelessWidget {
       ),
       body: SafeArea(
         child: Obx(
-          () => ListView.separated(
-            itemCount: ctrl.likeCount,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(ctrl.likes.value[index].content),
-                onTap: () => Get.toNamed(AppRouter.likeDetail,
-                    arguments: ctrl.likes.value[index].id),
-              );
-            },
-            separatorBuilder: (context, index) => Divider(thickness: 0.4),
-          ),
+          () => homeCtrl.likeCount > 0
+              ? ListView.separated(
+                  itemCount: homeCtrl.likeCount,
+                  itemBuilder: (context, index) {
+                    final joke = homeCtrl.likes.value[index];
+                    return Dismissible(
+                      direction: DismissDirection.endToStart,
+                      background: Container(
+                        color: Colors.red,
+                        alignment: Alignment.centerRight,
+                        padding: const EdgeInsets.only(right: 16),
+                        child: Icon(Icons.delete, color: Colors.white),
+                      ),
+                      key: Key(joke.id),
+                      onDismissed: (_) => homeCtrl.removeLike(joke.id),
+                      // direction: DismissDirection.endToStart,
+                      child: ListTile(
+                        title: Text(joke.content),
+                        contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 8),
+                        onTap: () => Get.toNamed(AppRouter.likeDetail,
+                            arguments: joke.id),
+                      ),
+                    );
+                  },
+                  separatorBuilder: (context, index) =>
+                      Divider(thickness: 0.4, height: 0),
+                )
+              : Center(
+                  child: Text('Chuck Norris has no jokes'),
+                ),
         ),
       ),
     );
